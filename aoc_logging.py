802b265919707b88678e80501490b43872c1f76f
Jiahe <yi.jiahe00@gmail.com>
2021-12-06 13:13:04 +0800

    Update logger

--- aoc_logging.py
+++ aoc_logging.py
@@ -3,5 +3,5 @@ import logging
 logger = logging.getLogger("logger")
 logger.setLevel(logging.INFO)
 
-handler = logging.FileHandler("info.log")
+handler = logging.FileHandler("info.log", mode='w')
 logger.addHandler(handler)
