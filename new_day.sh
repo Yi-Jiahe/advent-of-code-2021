69ded12bdc675773d1e65d9a5462780caa5a2d5d
Jiahe <yi.jiahe00@gmail.com>
2021-12-12 13:16:46 +0800

    Add error handling to new_day.sh

--- new_day.sh
+++ new_day.sh
@@ -1,7 +1,17 @@
-DAY=$1
+if [[ -z $1 ]]; then
+  echo "Please provide a day"
+  exit 1
+fi
 
-mkdir "day_$1"
-touch "day_$1/__init__.py"
-cp template.py "day_$1/main.py"
-touch "day_$1/puzzle_input.txt"
-touch "tests/test_day_$1.py"
+day=$1
+
+if [ -d "day_${day}" ]; then
+  echo "Directory already exists for day ${day}"
+  exit 1
+fi
+
+mkdir "day_${day}"
+touch "day_${day}/__init__.py"
+cp template.py "day_${day}/main.py"
+touch "day_${day}/puzzle_input.txt"
+touch "tests/test_day_${day}.py"
