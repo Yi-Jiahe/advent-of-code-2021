2bde9caf75e268aa0e9ffdeacb83426e5f82768b
Jiahe <yi.jiahe00@gmail.com>
2021-12-20 18:31:55 +0800

    finish day 19 part 2

--- day_19/main.py
+++ day_19/main.py
@@ -36,6 +36,10 @@ class Scanner:
     def __init__(self, id):
         self.id = id
         self.beacons = []
+        self.offset = None
+        self.rotation = None
+
+        self.no_match = set()
 
     def __str__(self):
         s = f"------ Scanner {self.id} ---------\n"
@@ -63,13 +67,6 @@ class Scanner:
                             continue
 
                         absolute_position = to_absolute_position(R, beacon, offset)
-                        outside_detection_range = False
-                        for i in range(3):
-                            if abs(absolute_position[i] - offset[i]) > 1000:
-                                outside_detection_range = True
-                                break
-                        if outside_detection_range:
-                            continue
 
                         if absolute_position in to_compare:
                             number_matched += 1
@@ -85,6 +82,45 @@ class Scanner:
 
                     offsets_checked.add(tuple(offset))
         return False, known_beacons
+
+    def match_scanner(self, other):
+        if other.id in self.no_match:
+            return False
+
+        for R in generate_rotation_matrices():
+            offsets_checked = set()
+            for base_beacon_relative_self in self.beacons:
+                for base_beacon_relative_other in other.beacons:
+                    offset = np.array(to_absolute_position(other.rotation, base_beacon_relative_other, other.position))\
+                             - np.array(to_absolute_position(R, base_beacon_relative_self, np.zeros(3)))
+
+                    if tuple(offset) in offsets_checked:
+                        continue
+
+                    number_matched = 1
+                    to_compare = {to_absolute_position(other.rotation, beacon, other.position) for beacon in other.beacons if beacon != base_beacon_relative_other}
+
+                    # Match other beacons
+                    for beacon in self.beacons:
+                        if beacon == base_beacon_relative_self:
+                            continue
+
+                        absolute_position = to_absolute_position(R, beacon, offset)
+
+                        if absolute_position in to_compare:
+                            number_matched += 1
+                            to_compare.remove(absolute_position)
+
+                        if number_matched >= 12:
+                            self.position = offset
+                            self.rotation = R
+
+                            return True
+
+                    offsets_checked.add(tuple(offset))
+
+        self.no_match.add(other.id)
+        return False
 
 
 def parse_input(iterable):
@@ -142,14 +178,47 @@ def part_one(scanners):
     return answer
 
 
-def part_two():
+def part_two(scanners):
     logger.info(title)
     logger.info("--- Part Two ---")
     print("--- Part Two ---")
 
+    determined_scanners = []
 
+    # Use scanner 0 as reference
+    scanner = scanners.pop(0)
+    scanner.position = np.array([0, 0, 0])
+    scanner.rotation = np.array([[1, 0, 0],
+                                 [0, 1, 0],
+                                 [0, 0, 1]])
+    determined_scanners.append(scanner)
+
+    while scanners:
+        scanner = scanners.pop(0)
+        matched = False
+        for other in determined_scanners:
+            matched = scanner.match_scanner(other)
+            if matched:
+                break
+        if matched:
+            print(scanner.id)
+            determined_scanners.append(scanner)
+        else:
+            scanners.append(scanner)
+
+    max_distance = 0
+    for i, scanner_a in enumerate(determined_scanners):
+        for scanner_b in determined_scanners[i+1:]:
+            manhattan_distance = 0
+            for j in range(3):
+                manhattan_distance += int(abs(scanner_a.position[j] - scanner_b.position[j]))
+            max_distance = max(manhattan_distance, max_distance)
+
+    print(max_distance)
 
     logger.info("")
+
+    return max_distance
 
 
 if __name__ == '__main__':
@@ -159,5 +228,8 @@ if __name__ == '__main__':
     with(open("puzzle_input.txt", 'r')) as f:
         scanners = parse_input(f)
 
-    part_one(copy.deepcopy(scanners))
-    part_two()
+    # part_one(copy.deepcopy(scanners))
+    # Part 2 assumes you have not already placed all the scanners as you would have in part 1.
+    # The reason for that is that I decided to stick to the question a bit closer and compare scanners to other scanners
+    # thinking that it might be faster, which does not seem to be the case.
+    part_two(copy.deepcopy(scanners))
